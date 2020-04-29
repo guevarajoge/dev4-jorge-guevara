0e69a40780ba8e6c7f9744b9456652049db7a917
Jorge Guevara <jorgeguevara_16@Jorges-MBP.lan>
2020-04-29 04:42:01 +0200

    test the master repo

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@
 // import literal from './modules/oBject.js';
 // import aRra from './modules/aRray.js';
 import config from '../config.js'; // json exported as module 
+console.log('test the state of the repo');
 
 let items = config.items;
 items.forEach(e=>console.log(`genre: ${e.genre} & category: ${e.category}`));
