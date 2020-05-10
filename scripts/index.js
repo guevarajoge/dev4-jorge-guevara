5ede7d92fb9d68af2d479da21966e3c33aad3363
Jorge Guevara <jorgeguevara_16@Jorges-MacBook-Pro.local>
2020-05-10 14:39:15 +0200

    3.1 fetching data with jQuery

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,38 @@
+//testing modules
 // import {Car} from './modules/carClass.js'; 
 // import {sayHi} from './modules/fUnction.js';
 // import literal from './modules/oBject.js';
 // import aRra from './modules/aRray.js';
-import config from '../config.js'; // json exported as module 
-console.log('test the state of the repo');
 
-let items = config.items;
-items.forEach(e=>console.log(`genre: ${e.genre} & category: ${e.category}`));
-    
-// });
-console.log(config.items);
+
+
+// import config from '../config.js'; // json exported as module 
+// console.log('test the state of the repo');
+
+// let items = config.items;
+// items.forEach(e=>console.log(`genre: ${e.genre} & category: ${e.category}`));
+
+// // });
+// console.log(config.items);
+
+//transversing the folder hierarchy for the url seams to be with ./ == up one level  instead of ../ 
+$(function () {
+    $.ajax({
+
+        url: "./entries.json",
+        method: 'GET',
+        dataType: 'json',
+    }).done(function (data) {
+        console.log(data);
+        callLater(data);
+    });
+
+
+    function callLater(data) {
+        $('#results').text(data.items[0]["genre-v2"]);
+
+    }
+
+
+
+});
